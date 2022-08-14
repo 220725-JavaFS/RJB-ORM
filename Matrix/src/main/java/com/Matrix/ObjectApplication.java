736b62d28230baba22930461a94c35eec7c6fda0
Ryan Brooks <RJBComputerScientist@gmail.com>
2022-08-14 19:50:12 -0400

    New Matrix project with spring boot

---
+++ Matrix/src/main/java/com/Matrix/ObjectApplication.java
@@ -0,0 +1,14 @@
+package com.Matrix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObjectApplication {
+
+	public static void main(String[] args) {
+//		SpringApplication.run(ObjectApplication.class, args);
+		System.out.println("SPRING BOOT FRAMEWORK");
+	}
+
+}
---
+++ Matrix/src/test/java/com/Matrix/ObjectApplicationTests.java
@@ -0,0 +1,14 @@
+package com.Matrix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+		
+	}
+
+}
